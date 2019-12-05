b29a3d40d16c4556aab039952d63728faa3b2e88
Valeria Serci <balerty84@gmail.com>
2019-12-05 18:12:39 +0000

    Posts are coming from Redux

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
+    UNSAFE_componentWillMount() {
+        this.props.fetchPosts();
+    }
+    
     render() {
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -17,5 +23,9 @@ class Posts extends Component {
     }
 }
 
+const mapStateToProps = state => ({
+    posts: state.posts.items
+});
 
-export default Posts;
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
